814145d50f5ad2bbcad123f4edf8be48b4d141e4
Scoti Dodson <scotidodson@gmail.com>
2018-06-30 22:13:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Scoti"
 var height = "74"
 
 console.log("Name:")
@@ -8,5 +8,3 @@ console.log("name")
 
 console.log("Height:")
 console.log(height)
-
-var name = "Scoti"
